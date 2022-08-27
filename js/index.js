ae45e1bc606e818677d9e6867a6f1fefc8174466
Alex-McAvoy <931772462@qq.com>
2022-08-27 15:45:41 +0800

    [Update] 更新了时间轴

--- js/index.js
+++ js/index.js
@@ -7,6 +7,24 @@ app.controller('HomeController', function($scope) {
 	//图标显示
 	$scope.goal_20200726 = true;
 	$scope.goal_20200824 = true;
+	$scope.goal_20200903 = true;
+	$scope.goal_20200905 = true;
+	$scope.goal_20200920 = true;
+	$scope.goal_20201003 = true;
+	$scope.goal_20201114 = true;
+	$scope.goal_20210101 = true;
+	$scope.goal_20210202 = true;
+	$scope.goal_20210214 = true;
+	$scope.goal_20210408 = true;
+	$scope.goal_20210520 = true;
+	$scope.goal_20210626 = true;
+	$scope.goal_20210814 = true;
+	$scope.goal_20210905 = true;
+	$scope.goal_20211120 = true;
+	$scope.goal_20220106 = true;
+	$scope.goal_20220123 = true;
+	$scope.goal_20220716 = true;
+	$scope.goal_20220905 = true;
 
 });
 
@@ -52,5 +70,186 @@ $(document).ready(function(e) {
 			});
 		}).fadeTo('slow', 2);
 	});
+	
+	//2020-09-03
+	$("#goal_20200903").click(function() {
+		$("body").fadeTo("ease", 0.3, function() {
+			$(this).css({
+				"background": "url(./img/20200903.jpg) no-repeat fixed",
+				"background-size": "cover"
+			});
+		}).fadeTo('slow', 2);
+	});
+	
+	//2020-09-05
+	$("#goal_20200905").click(function() {
+		$("body").fadeTo("ease", 0.3, function() {
+			$(this).css({
+				"background": "url(./img/20200905.jpg) no-repeat fixed",
+				"background-size": "cover"
+			});
+		}).fadeTo('slow', 2);
+	});
+	
+	//2020-09-20
+	$("#goal_20200920").click(function() {
+		$("body").fadeTo("ease", 0.3, function() {
+			$(this).css({
+				"background": "url(./img/20200920.jpg) no-repeat fixed",
+				"background-size": "cover"
+			});
+		}).fadeTo('slow', 2);
+	});
+	
+	//2020-10-03
+	$("#goal_20201003").click(function() {
+		$("body").fadeTo("ease", 0.3, function() {
+			$(this).css({
+				"background": "url(./img/20201003.jpg) no-repeat fixed",
+				"background-size": "cover"
+			});
+		}).fadeTo('slow', 2);
+	});
+	
+	//2020-11-14
+	$("#goal_20201114").click(function() {
+		$("body").fadeTo("ease", 0.3, function() {
+			$(this).css({
+				"background": "url(./img/20201114.jpg) no-repeat fixed",
+				"background-size": "cover"
+			});
+		}).fadeTo('slow', 2);
+	});
+	
+	//2021-01-01
+	$("#goal_20210101").click(function() {
+		$("body").fadeTo("ease", 0.3, function() {
+			$(this).css({
+				"background": "url(./img/20210101.jpg) no-repeat fixed",
+				"background-size": "cover"
+			});
+		}).fadeTo('slow', 2);
+	});
+	
+	//2021-02-02
+	$("#goal_20210202").click(function() {
+		$("body").fadeTo("ease", 0.3, function() {
+			$(this).css({
+				"background": "url(./img/20210202.jpg) no-repeat fixed",
+				"background-size": "cover"
+			});
+		}).fadeTo('slow', 2);
+	});
+	
+	//2021-02-14
+	$("#goal_20210214").click(function() {
+		$("body").fadeTo("ease", 0.3, function() {
+			$(this).css({
+				"background": "url(./img/20210214.jpg) no-repeat fixed",
+				"background-size": "cover"
+			});
+		}).fadeTo('slow', 2);
+	});
+	
+	//2021-04-08
+	$("#goal_20210408").click(function() {
+		$("body").fadeTo("ease", 0.3, function() {
+			$(this).css({
+				"background": "url(./img/20210408.jpg) no-repeat fixed",
+				"background-size": "cover"
+			});
+		}).fadeTo('slow', 2);
+	});
+	
+	//2021-05-20
+	$("#goal_20210520").click(function() {
+		$("body").fadeTo("ease", 0.3, function() {
+			$(this).css({
+				"background": "url(./img/20210520.jpg) no-repeat fixed",
+				"background-size": "cover"
+			});
+		}).fadeTo('slow', 2);
+	});
+	
+	//2021-06-26
+	$("#goal_20210626").click(function() {
+		$("body").fadeTo("ease", 0.3, function() {
+			$(this).css({
+				"background": "url(./img/20210626.jpg) no-repeat fixed",
+				"background-size": "cover"
+			});
+		}).fadeTo('slow', 2);
+	});
+	
+	//2021-08-14
+	$("#goal_20210814").click(function() {
+		$("body").fadeTo("ease", 0.3, function() {
+			$(this).css({
+				"background": "url(./img/20210814.jpg) no-repeat fixed",
+				"background-size": "cover"
+			});
+		}).fadeTo('slow', 2);
+	});
+	
+	//2021-09-05
+	$("#goal_20210905").click(function() {
+		$("body").fadeTo("ease", 0.3, function() {
+			$(this).css({
+				"background": "url(./img/20210905.jpg) no-repeat fixed",
+				"background-size": "cover"
+			});
+		}).fadeTo('slow', 2);
+	});
+	
+	//2021-11-20
+	$("#goal_20211120").click(function() {
+		$("body").fadeTo("ease", 0.3, function() {
+			$(this).css({
+				"background": "url(./img/20211120.jpg) no-repeat fixed",
+				"background-size": "cover"
+			});
+		}).fadeTo('slow', 2);
+	});
+	
+	//2022-01-06
+	$("#goal_20220106").click(function() {
+		$("body").fadeTo("ease", 0.3, function() {
+			$(this).css({
+				"background": "url(./img/20220106.jpg) no-repeat fixed",
+				"background-size": "cover"
+			});
+		}).fadeTo('slow', 2);
+	});
+	
+	//2022-01-23
+	$("#goal_20220123").click(function() {
+		$("body").fadeTo("ease", 0.3, function() {
+			$(this).css({
+				"background": "url(./img/20220123.jpg) no-repeat fixed",
+				"background-size": "cover"
+			});
+		}).fadeTo('slow', 2);
+	});
+	
+	//2022-07-16
+	$("#goal_20220716").click(function() {
+		$("body").fadeTo("ease", 0.3, function() {
+			$(this).css({
+				"background": "url(./img/20220716.jpg) no-repeat fixed",
+				"background-size": "cover"
+			});
+		}).fadeTo('slow', 2);
+	});
+	
+	//2022-09-05
+	$("#goal_20220905").click(function() {
+		$("body").fadeTo("ease", 0.3, function() {
+			$(this).css({
+				"background": "url(./img/20220905.jpg) no-repeat fixed",
+				"background-size": "cover"
+			});
+		}).fadeTo('slow', 2);
+	});
+	
 
 });
